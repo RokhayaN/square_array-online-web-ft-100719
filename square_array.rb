ca164d81de7891d7b5b4aeace834f0bc1800cd1e
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 05:44:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,5 +2,5 @@ def
   square_array = [2,3,4]
   number_i = [2,3,4]
   number_f = [4,9,16]
-  number_i.each ** number_i.each == |number_f|.each
+  number_i.each ** number_i.each == |number_f|.size
 end
